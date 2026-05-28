47448685373a6abf48f953ab3010ee671f07bdb1
Xavier Mas Leszkiewicz <xavier.mas.le@gmail.com>
2026-05-28 17:44:37 +0100

    3.6.0

--- plan-ai-mobile/app.config.ts
+++ plan-ai-mobile/app.config.ts
@@ -18,7 +18,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
   // ----------------------------------
 
   const isProduction = process.env.APP_ENV === "production";
-  const appVersion = "3.5.0";
+  const appVersion = "3.6.0";
   const bundleIdentifier = "com.blueberrybytes.planai";
 
   return {
