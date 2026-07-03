d5ef811a414b6e0650818f72e5945272a451d2a0
Xavier Mas Leszkiewicz <xavier.mas.le@gmail.com>
2026-07-03 15:31:34 +0200

    v

--- plan-ai-recorder/electron-builder.config.js
+++ plan-ai-recorder/electron-builder.config.js
@@ -34,7 +34,7 @@ module.exports = {
       channel: isHouseGroup ? "housegroup" : "latest",
     },
   ],
-  buildVersion: "160",
+  buildVersion: "161",
   protocols: [
     {
       name: productName,
